4b49577e5154c688e5e34861e2caf80237efd11f
Rene Hexel <r.hexel@griffith.edu.au>
2016-09-26 10:02:24 +0200

    Add pango to pkg-config

--- config.sh
+++ config.sh
@@ -8,5 +8,5 @@ Module=${Mod}-1.0
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
 mod=`echo "${Mod}" | tr '[:upper:]' '[:lower:]'`
 export PATH=`pwd`/.build/gir2swift/.build/debug:"${PATH}"
-LINKFLAGS=`pkg-config --libs $module gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
-CCFLAGS=`pkg-config --cflags $module gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
+LINKFLAGS=`pkg-config --libs $module pango gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
+CCFLAGS=`pkg-config --cflags $module pango gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
