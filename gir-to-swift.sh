bc41ee8c0fcde4844dac6147c0439a2ac0ac99b8
Rene Hexel <r.hexel@griffith.edu.au>
2016-09-28 20:39:51 +0200

    Generate an individual .swift file for each type

--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -21,7 +21,12 @@ if [ ! -e "${GIR}" ] ; then
 	echo "and can be found in /usr /usr/local or by pkg-config!"
 	exit 1
 fi
-gir2swift -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/cairo-1.0.gir -p ${GIR_DIR}/Pango-1.0.gir -p ${GIR_DIR}/Cogl-1.0.gir -p ${GIR_DIR}/CoglPango-1.0.gir "${GIR}" | sed -f ${Module}.sed > Sources/${Module}.swift
+gir2swift -o Sources -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/cairo-1.0.gir -p ${GIR_DIR}/Pango-1.0.gir -p ${GIR_DIR}/Cogl-1.0.gir -p ${GIR_DIR}/CoglPango-1.0.gir "${GIR}"
+for src in Sources/*-*.swift ; do
+	sed -f ${Module}.sed < ${src} > ${src}.out
+	mv ${src}.out ${src}
+done
+touch Sources/${Module}.swift
 #echo  > Sources/Swift${Mod}.swift "import CGLib"
 #echo  > Sources/Swift${Mod}.swift "import CCairo"
 #echo >> Sources/Swift${Mod}.swift "import GLib"
