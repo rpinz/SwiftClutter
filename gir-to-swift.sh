8842b3906fd6e02b6ad49265e791e545a21cff44
Rene Hexel <r.hexel@griffith.edu.au>
2017-05-14 08:08:20 +1000

    Use gir2swift -m option

--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -21,7 +21,7 @@ if [ ! -e "${GIR}" ] ; then
 	echo "and can be found in /usr /usr/local or by pkg-config!"
 	exit 1
 fi
-gir2swift -o Sources -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/cairo-1.0.gir -p ${GIR_DIR}/Pango-1.0.gir -p ${GIR_DIR}/Cogl-1.0.gir -p ${GIR_DIR}/CoglPango-1.0.gir "${GIR}"
+gir2swift -o Sources -m ${Module}.module -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/cairo-1.0.gir -p ${GIR_DIR}/Pango-1.0.gir -p ${GIR_DIR}/Cogl-1.0.gir -p ${GIR_DIR}/CoglPango-1.0.gir "${GIR}"
 for src in Sources/*-*.swift ; do
 	sed -f ${Module}.sed < ${src} > ${src}.out
 	mv ${src}.out ${src}
