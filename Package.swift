bef6bb2bb5f962566316fbf7bb2454f395322e48
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-28 07:41:30 +1000

    Compile with SwiftLanguageVersion 3 under Swift 4

--- Package.swift
+++ Package.swift
@@ -6,5 +6,6 @@ let package = Package(
         .Package(url: "https://github.com/rhx/CClutter.git", majorVersion: 1),
         .Package(url: "https://github.com/rhx/SwiftCairo.git", majorVersion: 2),
         .Package(url: "https://github.com/rhx/SwiftCoglPango.git", majorVersion: 2),
-    ]
+    ],
+    swiftLanguageVersions: [3]
 )
