e07726a25b64d070acf20bf42ef8f91f922a34e9
Ron Pinz <ron.pinz@gmail.com>
2018-11-23 19:47:13 -0500

    add manifest for swift 5

---
+++ Package@swift-5.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Clutter",
+    products: [
+        .library(
+            name: "Clutter",
+            targets: ["Clutter"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/rpinz/SwiftCairo",
+            .branch("master")),
+        .package(
+            url: "https://github.com/rpinz/SwiftCoglPango",
+            .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "Clutter",
+            dependencies: ["CClutter","Cairo", "CoglPango"],
+            path: "Sources"),
+        .systemLibrary(
+            name: "CClutter",
+            path: "Library/CClutter",
+            pkgConfig: "clutter-1.0",
+            providers: [
+                .brew(["libclutter-1.0"]),
+                .apt(["libclutter-1.0-dev"])
+            ])
+    ],
+    swiftLanguageVersions: [
+        .v4_2
+    ]
+)
